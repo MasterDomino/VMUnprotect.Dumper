fe95fed9bd90b44b2791e3f850875b85b284d4d2
Dominik Grudziński <dominik.grudzinski@hubersuhner.com>
2024-10-07 13:34:45 +0200

    remove old project files

--- VMUnprotect.Dumper/Program.cs
+++ VMUnprotect.Dumper/Program.cs
@@ -4,9 +4,14 @@ using System.Runtime.InteropServices;
 using AsmResolver.DotNet;
 using AsmResolver.IO;
 using AsmResolver.PE.File;
+using AsmResolver.PE.File.Headers;
 using Sharprompt;
 
-const string asciiArt = @"
+namespace VMProtect.Dumper
+{
+	internal class Program
+	{
+		const string asciiArt = @"
 _________                __                 
 \_   ___ \  ____________/  |_  ____ ___  ___
 /    \  \/ /  _ \_  __ \   __\/ __ \\  \/  /
@@ -14,85 +19,97 @@ _________                __
  \______  /\____/|__|   |__|  \___  >__/\_ \
         \/                        \/      \/
                 VMUnprotect.Dumper
-          https://github.com/void-stack
-            Credits: wwh1004, MrToms
+          https://github.com/404
+            Credits: wwh1004, MrToms, void-stack,TheVoid
 ";
+		static void Main(string[] args)
+		{
+			Console.Title = "VMUnprotect.Dumper";
+			Console.WriteLine(asciiArt);
 
-Console.Title = "VMUnprotect.Dumper";
-Console.WriteLine(asciiArt);
+			string? target;
 
-string? target;
+			if (args.Length > 0 && File.Exists(args[0]))
+			{
+				target = Prompt.Input<string>("Enter file path", args[0]);
+			}
+			else
+			{
+				target = Prompt.Input<string>("Enter file path");
+			}
 
-if (args.Length > 0 && File.Exists(args[0]))
-    target = Prompt.Input<string>("Enter file path", args[0]);
-else
-    target = Prompt.Input<string>("Enter file path");
+			// adjust path to absolute, required by Assembly.LoadFile();
+			target = Path.GetFullPath(target);
 
-if (File.Exists(target))
-{
-    var output = $"{Path.GetFileNameWithoutExtension(target)}-decrypted.exe";
+			if (File.Exists(target))
+			{
+				string fileName = $"{Path.GetFileNameWithoutExtension(target)}-decrypted.exe";
 
-    // Try to load assembly and gather the ManifestModule
-    Assembly? assembly = null;
+				// Try to load assembly and gather the ManifestModule
+				Assembly? assembly;
 
-    try
-    {
-        assembly = Assembly.LoadFile(target);
-    }
-    catch (BadImageFormatException)
-    {
-        Console.WriteLine("Target app probably has a different framework.");
-        return;
-    }
+				try
+				{
+					assembly = Assembly.LoadFile(target);
+				}
+				catch (BadImageFormatException)
+				{
+					Console.WriteLine("Target app probably has a different framework.");
+					return;
+				}
 
-    var manifestModule = assembly.ManifestModule;
+				Module manifestModule = assembly.ManifestModule;
 
-    // Quick load for .cctor search
-    var module = ModuleDefinition.FromFile(target);
+				// Quick load for .cctor search
+				ModuleDefinition module = ModuleDefinition.FromFile(target);
 
-    // Resolve MethodBase of .cctor where vmp initializes itself
-    var cctor =
-        assembly.ManifestModule.ResolveMethod(module.TopLevelTypes[0].GetStaticConstructor()!.MetadataToken.ToInt32());
+				// Resolve MethodBase of .cctor where vmp initializes itself
+				MethodBase? cctor =
+					assembly.ManifestModule.ResolveMethod(module.TopLevelTypes[0].GetStaticConstructor()!.MetadataToken.ToInt32());
 
-    // Get Module Base Address from loaded assembly
-    var hInstance = Marshal.GetHINSTANCE(manifestModule);
+				// Get Module Base Address from loaded assembly
+				nint hInstance = Marshal.GetHINSTANCE(manifestModule);
 
-    // Make sure static constructor exists
-    if (cctor is not null)
-    {
-        // Force VMProtect to fix methods
-        RuntimeHelpers.PrepareMethod(cctor.MethodHandle);
+				// Make sure static constructor exists
+				if (cctor is not null)
+				{
+					// TODO: This will error out if assembly has anti-debug implemented
+					// Force VMProtect to fix methods
+					RuntimeHelpers.PrepareMethod(cctor.MethodHandle);
 
-        // Load PEFile from disk
-        var diskImage = PEFile.FromFile(target);
+					// Load PEFile from disk
+					PEFile diskImage = PEFile.FromFile(target);
 
-        // Get correct AddressOfEntrypoint and fix determining whether the image is a PE32 (32-bit) or a PE32+ (64-bit) image.
-        var epFromDisk = diskImage.OptionalHeader.AddressOfEntrypoint;
-        var magicDisk = diskImage.OptionalHeader.Magic;
+					// Load decrypted PEFile from module base
+					PEFile runtimeImage = PEFile.FromModuleBaseAddress(hInstance);
 
-        // Load decrypted PEFile from module base
-        var runtimeImage = PEFile.FromModuleBaseAddress(hInstance, PEMappingMode.Mapped);
+					OptionalHeader optionalHeader = runtimeImage.OptionalHeader;
+					optionalHeader.Magic = diskImage.OptionalHeader.Magic; // Fix the incorrect magic
+					optionalHeader.AddressOfEntryPoint = diskImage.OptionalHeader.AddressOfEntryPoint; // Fix the incorrect AddressOfEntrypoint
 
-        var optionalHeader = runtimeImage.OptionalHeader;
-        optionalHeader.Magic = magicDisk; // Fix the incorrect magic
-        optionalHeader.AddressOfEntrypoint = epFromDisk; // Fix the incorrect AddressOfEntrypoint
+					// try to do onje of the following to remove unnecessary garbage 
+					//runtimeImage.AlignSections();
+					//runtimeImage.UpdateHeaders();
 
-        // Write fixed runtimeImage to disk
-        using (var fs = File.Create(output))
-        {
-            runtimeImage.Write(new BinaryStreamWriter(fs));
-        }
+					// Write fixed runtimeImage to disk
+					using (FileStream fs = File.Create(fileName))
+					{
+						runtimeImage.Write(new BinaryStreamWriter(fs));
+					}
 
-        Console.WriteLine($"Saved as: {Path.GetFullPath(output)}");
-    }
-    else
-    {
-        Console.WriteLine("Failed to prepare .cctor");
-    }
+					Console.WriteLine($"Saved as: {Path.GetFullPath(fileName)}");
+				}
+				else
+				{
+					Console.WriteLine("Failed to prepare .cctor");
+				}
+			}
+			else
+			{
+				Console.WriteLine("File either doesn't exist or you didn't provide it (VMProtect.Dumper File.exe)");
+			}
+
+			Console.ReadKey();
+		}
+	}
 }
-else
-{
-    Console.WriteLine("File either doesn't exist or you didn't provide it (VMProtect.Dumper File.exe)");
-}
-
-Console.ReadKey();
